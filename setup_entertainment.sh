924f5499907d0dc20a3ccdee56ea2dd162ca0411
Trey Watford <treyjustinwatford@gmail.com>
2024-07-20 09:48:33 -0400

    added steam but commented out until i do some more research on it

--- setup_entertainment.sh
+++ setup_entertainment.sh
@@ -5,6 +5,7 @@
 # author: Trey Watford (treyjustinwatford@gmail.com)
 # usage:
 #   This script installs some apps for entertainment like battle.net
+#   and steam
 ####################################################################
 # variables
 OS_LINUX=0
@@ -21,6 +22,8 @@ if [[ "${OS_LINUX}" -eq 1 ]]; then
     echo "Linux detected, but installation steps are not defined."
 else
     # install using homebrew cask for mac
-    brew install --cask battle-net # install battle.net desktop app
+    brew install --cask battle-net
     echo "to install batte.net make sure to run 'open and the path to the .app file'"
+    # brew install --cask steam
+    # steam is built for intel based macs and requires rosetta 2 which is apparently hard to remove after install so i'm not installing it yet
 fi
